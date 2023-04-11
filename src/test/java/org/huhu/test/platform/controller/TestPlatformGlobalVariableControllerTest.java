48a18f3b0a28bc73d45b27d6d7646ba3b1f57a3d
chen <18551681083@163.com>
2023-04-11 23:00:12 +0800

    完成成功创建测试平台全局变量的单元测试

---
+++ src/test/java/org/huhu/test/platform/controller/TestPlatformGlobalVariableControllerTest.java
@@ -0,0 +1,58 @@
+package org.huhu.test.platform.controller;
+
+import org.huhu.test.platform.model.request.GlobalVariableModifyRequest;
+import org.huhu.test.platform.model.response.GlobalVariableModifyResponse;
+import org.huhu.test.platform.model.vo.GlobalVariableCreateVo;
+import org.huhu.test.platform.service.TestPlatformGlobalVariableService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WithMockUser
+@WebFluxTest(TestPlatformGlobalVariableController.class)
+class TestPlatformGlobalVariableControllerTest {
+
+    @MockBean
+    TestPlatformGlobalVariableService globalVariableService;
+
+    @Autowired
+    WebTestClient webClient;
+
+    @Test
+    void query() {
+    }
+
+    @Test
+    void create() {
+        var response = new GlobalVariableModifyResponse("someName", "someValue", "someDescription");
+        Mockito.doReturn(Mono.just(response))
+               .when(globalVariableService)
+               .createTestPlatformGlobalVariable(ArgumentMatchers.any(GlobalVariableCreateVo.class));
+        var request = new GlobalVariableModifyRequest("someName", "someValue", "someDescription");
+        webClient.mutateWith(SecurityMockServerConfigurers.csrf())
+                 .put()
+                 .uri("/global/variable")
+                 .bodyValue(request)
+                 .exchange()
+                 .expectStatus().isOk()
+                 .expectBody()
+                 .jsonPath("$.variableName").isEqualTo("someName")
+                 .jsonPath("$.variableValue").isEqualTo("someValue")
+                 .jsonPath("$.variableDescription").isEqualTo("someDescription");
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
