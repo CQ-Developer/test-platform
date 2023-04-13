78c117a86fc8904d7100988af9a3e8ebe295eeaa
qchen71 <qchen71@ford.com>
2023-04-13 12:40:16 +0800

    为TestPlatformError添加代码注释

--- src/main/java/org/huhu/test/platform/constant/TestPlatformError.java
+++ src/main/java/org/huhu/test/platform/constant/TestPlatformError.java
@@ -1,17 +1,46 @@
 package org.huhu.test.platform.constant;
 
+/**
+ * 测试平台错误码枚举类
+ *
+ * @author 18551681083@163.com
+ * @since 0.0.1
+ */
 public enum TestPlatformError {
 
+    /**
+     * 客户端请求错误
+     */
     CLIENT_ERROR(1000, "client request error"),
+
+    /**
+     * 客户端请求参数错误
+     */
     CLIENT_REQUEST_PARAMETER_INVALID(1001, "request parameter invalid"),
+
+    /**
+     * 客户端请求的用户名非法
+     */
     CLIENT_REQUEST_USERNAME_INVALID(1002, "request username invalid"),
 
+    /**
+     * 第三方服务错误
+     */
     THIRD_PARTY_SERVICE_ERROR(2000, "third party service error"),
 
+    /**
+     * 服务端错误
+     */
     SERVER_ERROR(3000, "server occurred an unknown error");
 
+    /**
+     * 错误码
+     */
     final int errorCode;
 
+    /**
+     * 错误信息
+     */
     final String errorMessage;
 
     TestPlatformError(int errorCode, String errorMessage) {
@@ -19,10 +48,16 @@ public enum TestPlatformError {
         this.errorMessage = errorMessage;
     }
 
+    /**
+     * 获取错误码
+     */
     public int getErrorCode() {
         return errorCode;
     }
 
+    /**
+     * 获取错误信息
+     */
     public String getErrorMessage() {
         return errorMessage;
     }
