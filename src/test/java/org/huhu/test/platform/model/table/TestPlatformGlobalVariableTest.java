74050c3d653c917a9f3c74ea1af4e14b9f4ea1dc
qchen71 <qchen71@ford.com>
2023-04-14 17:35:28 +0800

    添加TestPlatformGlobalVariable单元测试

---
+++ src/test/java/org/huhu/test/platform/model/table/TestPlatformGlobalVariableTest.java
@@ -0,0 +1,12 @@
+package org.huhu.test.platform.model.table;
+
+/**
+ * 测试平台全局变量表单元测试
+ *
+ * @author 18551681083@163.com
+ * @see TestPlatformGlobalVariable
+ * @since 0.0.1
+ */
+class TestPlatformGlobalVariableTest {
+
+}
