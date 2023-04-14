638d56ca7eda6fc903267c767149528013015f01
qchen71 <qchen71@ford.com>
2023-04-14 09:34:23 +0800

    删除h2数据库

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("cn.hutool:hutool-all:5.8.17")
-    runtimeOnly("com.github.jasync-sql:jasync-r2dbc-mysql:2.1.23")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("io.r2dbc:r2dbc-h2")
+    runtimeOnly("com.github.jasync-sql:jasync-r2dbc-mysql:2.1.23")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<Test> {
