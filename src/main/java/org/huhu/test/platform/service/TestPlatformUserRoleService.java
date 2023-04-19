35dc20462d7ca7d990c843df50b4e81c49e339d4
qchen71 <qchen71@ford.com>
2023-04-19 11:04:18 +0800

    添加注释

--- src/main/java/org/huhu/test/platform/service/TestPlatformUserRoleService.java
+++ src/main/java/org/huhu/test/platform/service/TestPlatformUserRoleService.java
@@ -6,6 +6,12 @@ import org.huhu.test.platform.model.vo.UserRoleDeleteVo;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+/**
+ * 测试平台用户角色 {@link org.springframework.stereotype.Service} 接口
+ *
+ * @author 18551681083@163.com
+ * @since 0.0.1
+ */
 public interface TestPlatformUserRoleService {
 
     /**
