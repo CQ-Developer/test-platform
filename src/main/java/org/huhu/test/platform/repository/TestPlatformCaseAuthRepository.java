ed086aa4949696ff5c7efc3c9d1b3142b8ca857a
chen <18551681083@163.com>
2023-05-05 22:06:24 +0800

    定义 case auth repository 接口

---
+++ src/main/java/org/huhu/test/platform/repository/TestPlatformCaseAuthRepository.java
@@ -0,0 +1,13 @@
+package org.huhu.test.platform.repository;
+
+/**
+ * 测试平台用例认证 {@link org.springframework.data.repository.Repository} 接口
+ *
+ * @author 18551681083
+ * @since 0.0.2
+ */
+public interface TestPlatformCaseAuthRepository {
+
+    // todo 插入语句中需要进行加密
+
+}
