168fa15cd0df1cc04917d4450569ae9211c4a2fa
qchen71 <qchen71@ford.com>
2023-05-05 17:32:43 +0800

    定义密钥

---
+++ src/main/java/org/huhu/test/platform/constant/TestPlatformSecretKey.java
@@ -0,0 +1,35 @@
+package org.huhu.test.platform.constant;
+
+/**
+ * 测试平台密钥枚举类
+ *
+ * @author 18551681083@163.com
+ * @since 0.0.2
+ */
+public enum TestPlatformSecretKey {
+
+    /**
+     * 采用AES算法128位长度的密钥
+     */
+    AES_128("b54dab8e6c04cccfe4e89a8b8dd54494");
+
+    /**
+     * 16进制密钥
+     */
+    final String key;
+
+    /**
+     * 构造器函数
+     */
+    TestPlatformSecretKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * 获取16进制的密钥
+     */
+    public String getKey() {
+        return key;
+    }
+
+}
